fba2fdd607cc298104366a56085f09618aa03f35
rdelpapa27 <robertibackup@gmail.com>
2025-01-16 15:07:24 -0600

    Update Change One war_game.py

--- war_game.py
+++ war_game.py
@@ -4,6 +4,7 @@
 # This file contains information on a card and deck class.
 # Together we will build a player class
 # Then begin designing rules for a game
+# Test for change one
 
 # Global variables used to create a new deck
 face = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
